81cbb8b02fca3be01dc0304fc56a25c5c357bf19
takattata <ticktack016@gmail.com>
2017-09-01 18:51:55 +0900

    Initial commit

---
+++ notification-word/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  notification-word
+//
+//  Created by Takashima on 2017/09/01.
+//  Copyright © 2017年 takattata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
